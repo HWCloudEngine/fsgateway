ce9629397ae7397ee7d3efd0ebf7b111699fbda2
liulinghui <llhuii5614@gmail.com>
2016-01-08 10:12:45 +0800

    Comment database/tables in install.sh

--- fs_gateway/install.sh
+++ fs_gateway/install.sh
@@ -6,14 +6,12 @@ cp fs-gateway-api fs-gateway-proxy fs-gateway-manage /usr/bin
 mkdir -p /var/log/fusionsphere/component/fs-gateway
 
 # config file
-mkdir /etc/fs-gateway
-for f in fs-gateway.conf fs-gateway-paste.ini ; do
-    cp $f /etc/fs-gateway
-done 
+# mkdir /etc/fs-gateway
+# cp fs-gateway.conf fs-gateway-paste.ini /etc/fs-gateway
 
 # create fs_gateway database
-su gaussdba -c "cd;/opt/gaussdb/app/bin/gsql -W FusionSphere123  POSTGRES -c 'CREATE DATABASE fs_gateway OWNER openstack;' "
+#su gaussdba -c "cd;/opt/gaussdb/app/bin/gsql -W FusionSphere123  POSTGRES -c 'CREATE DATABASE fs_gateway OWNER openstack;' "
 
 # create fs_gateway db tables
-python ./fs-gateway-manage --config-file=/etc/fs-gateway/fs-gateway.conf db sync
+#python ./fs-gateway-manage --config-file=/etc/fs-gateway/fs-gateway.conf db sync
 
