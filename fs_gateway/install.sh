e73dd0c60697d2ac0f204d00d28eefa2600e5970
liulinghui <llhuii5614@gmail.com>
2016-01-08 11:46:57 +0800

    Correct database address in install.sh

--- fs_gateway/install.sh
+++ fs_gateway/install.sh
@@ -9,9 +9,13 @@ mkdir -p /var/log/fusionsphere/component/fs-gateway
 # mkdir /etc/fs-gateway
 # cp fs-gateway.conf fs-gateway-paste.ini /etc/fs-gateway
 
+# correct database address
+database="$(awk '/^connection *=/&&sub("nova$",e){print;exit}' /etc/nova/nova-api.conf)"
+sed -i 's!^connection *=.*$!'"$database"'fs_gateway!' fs-gateway.conf
+
 # create fs_gateway database
-#su gaussdba -c "cd;/opt/gaussdb/app/bin/gsql -W FusionSphere123  POSTGRES -c 'CREATE DATABASE fs_gateway OWNER openstack;' "
+su gaussdba -c "cd;/opt/gaussdb/app/bin/gsql -W FusionSphere123  POSTGRES -c 'CREATE DATABASE fs_gateway OWNER openstack;' "
 
 # create fs_gateway db tables
-#python ./fs-gateway-manage --config-file=/etc/fs-gateway/fs-gateway.conf db sync
+python ./fs-gateway-manage --config-file=/etc/fs-gateway/fs-gateway.conf db sync
 
